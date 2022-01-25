c73941263e2eed6bd1aa1787ccaa0fcb62bc0113
Tomen-coder <72608745+Tomen-coder@users.noreply.github.com>
2022-01-25 09:29:31 +0100

    Update App.js

--- documentation/src/App.js
+++ documentation/src/App.js
@@ -9,14 +9,14 @@ function App() {
       		<p>Aplikacja internetowa w środowisku Docker. Stworzona przy pomocy Docker, nodeJS oraz ReactJS. Zawiera ona kontener 'server' który rozdziela ruch na składowe kontenery aplikacji, czyli 			'documentation', 'home' oraz 'calculator'. Analizuje on ruch na podstawie żądania przychodzącego, a następnie dopasowuję ją do zadeklarowanego serwera 			upstream.</p>
       		<br><br>
       		<h2> Instalacja wersji Developerskiej: </h2>
-      		<p> W celu uruchomienia aplikacji niezbędnym jest wykonanie poniższych polecen.
+      		<p> W celu uruchomienia aplikacji niezbędnym jest wykonanie poniższych polecen.</p>
       		<br><br>
-      		 1. git clone https://github.com/tomen-coder/PFSwChO_zad1.git <br>
-      		 2. cp .env.example .env<br>
-      		 3. docker-compose -f docker-compose.dev.yml up --build --force-recreate
+      		 <p>1. git clone https://github.com/tomen-coder/PFSwChO_zad1.git  </p><br>
+      		 <p>2. cp .env.example .env </p><br>
+      		 <p>3. docker-compose -f docker-compose.dev.yml up --build --force-recreate </p>
       		 <br><br>
-      		 Po wykonaniu powyzszych działań można uruchomić przeglądarke i przejść pod adres http://localhost.
-      		 </p>
+      		<p> Po wykonaniu powyzszych działań można uruchomić przeglądarke i przejść pod adres http://localhost. </p>
+      		
       	</div>
     </div>
   );
